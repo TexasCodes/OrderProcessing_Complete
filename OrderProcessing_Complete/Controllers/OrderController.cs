2f4641186b70e7dfe346c6e4f4a0209130ce7730
Tiffany Selvidge <tselvidge@hotmail.com>
2021-08-04 09:52:04 -0500

    Update formatting

--- OrderProcessing_Complete/Controllers/OrderController.cs
+++ OrderProcessing_Complete/Controllers/OrderController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
--- OrderProcessing_Complete/Models/orderClass.cs
+++ OrderProcessing_Complete/Models/orderClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -10,27 +11,39 @@ namespace OrderProcessing_Complete.Models
 
         public int OrderID { get; set; }
 
+        [DisplayName("Order Date")]
         public string OrderDate { get; set; }
 
+        [DisplayName("Customer Name")]
         public string CustomerName { get; set; }
 
+        [DisplayName("Customer Address")]
         public string CustomerAddress { get; set; }
 
+        [DisplayName("Customer Phone")]
         public string CustomerPhone { get; set; }
 
+        [DisplayName("TShirt Color")]
         public string TShirtColor { get; set; }
 
+        [DisplayName("TShirt Size")]
         public string TShirtSize { get; set; }
 
+        [DisplayName("TShirt Price")]
         public string TShirtPrice { get; set; }
 
+        
         public string Quantity { get; set; }
 
+        [DisplayName("Order Total")]
         public int OrderTotal { get; set; }
 
+        [DisplayName("Order Status")]
         public int OrderStatusID { get; set; }
 
         public string Notes { get; set; }
+
+        public List<orderClass> statusInfo { get; set; }
 
     }
 }
